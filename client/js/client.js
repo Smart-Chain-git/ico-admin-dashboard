dff8dcbd5f39a43cb669ef7086304a3fbb24b1ea
CharlesAUGU <augucharles@gmail.com>
2021-04-17 17:37:47 +0200

    clean old content and set new html content

--- client/js/client.js
+++ client/js/client.js
@@ -2,4 +2,11 @@ console.log('Hello! I am client.js inside client/js/...');
 console.log('...This comment is on line 2 of client.js');
 
 // Insert the 'Hey there!' text on the template page
-document.getElementById('greeting').innerText = 'Hey there!';
+document.getElementById('greeting').innerText = 'Smartlink ICO dashboard';
+
+let x = document.querySelectorAll(".amount");
+        for (let i = 0, len = x.length; i < len; i++) {
+            let num = Number(x[i].innerHTML)
+                      .toLocaleString('en');
+            x[i].innerHTML = num;
+        }
