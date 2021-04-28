ff91d5f8c17be1d2d9beb57f651603b69b4d458a
CharlesAUGU <augucharles@gmail.com>
2021-04-28 11:10:50 +0200

    fetching delay ajustment

--- client/js/client.js
+++ client/js/client.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     const interval = setInterval(function() {
         getdata();
-    }, 5000);
+    }, 30000);
     getdata();
 
     function getdata(){  
@@ -12,19 +12,36 @@ $(document).ready(function(){
             dataType:'json',  
             success:function(response){
                 console.log(response.msg);
-                    if(response.msg=='success'){  
+                    //if(response.msg=='success'){  
                         $('#last_update').html(new Date());
                         console.log(response.d1);
+                        console.log(response.d2);
+                        console.log(response.d3);
+                        console.log(response.d4);
+                        console.log(response.d5);
+                        console.log(response.d6);
                         $('#validated').html(response.d1);
                         $('#validated_and_pending').html(response.d2);
                         $('#all_funds').html(response.d3);
+                        $('#presale_reserved').html(response.d5);
+                        $('#ico_reserved').html(response.d6);
+                        $('#total_reserved').html(response.d4);
+                        $('#presale_remaining').html(71432562-response.d5);
+                        $('#ico_remaining').html(200000000-response.d6);
+                        $('#total_remaining').html(271432562-response.d4);
                         let x = document.querySelectorAll(".amount");
                         for (let i = 0, len = x.length; i < len; i++) {
                             let num = Number(x[i].innerHTML)
                                     .toLocaleString('en');
                             x[i].innerHTML = num + "<span>&#36;</span>";
                         }
-                    }  
+                        let y = document.querySelectorAll(".smak");
+                        for (let i = 0, len = y.length; i < len; i++) {
+                            let num = Number(y[i].innerHTML)
+                                    .toLocaleString('en');
+                            y[i].innerHTML = num;
+                        }
+                    //}  
             },  
             error:function(response){  
                 alert('server error');
--- routes/taskroute.js
+++ routes/taskroute.js
@@ -5,11 +5,11 @@ const config = require('../config/config.js');
 var router = express.Router();  
 
 router.get('/getdata', async (req,res)=>{
-        var [data1,data2,data3] = await task();
-        if(!data1 || !data2 || !data3){
-            res.json({msg:'error'});
+        var [data1,data2,data3,data4,data5,data6] = await task();
+        if(!data1 || !data2 || !data3 || !data4 || !data5 || !data6){
+            res.json({msg:'error', d1:data1, d2:data2, d3:data3, d4:data4, d5:data5, d6:data6});
         }else{
-            res.json({msg:'success', d1:data1, d2:data2, d3:data3});
+            res.json({msg:'success', d1:data1, d2:data2, d3:data3, d4:data4, d5:data5, d6:data6});
         }
 });
 
@@ -27,15 +27,27 @@ function endDbConnection(connection_to_end)
 {connection_to_end.end();}
 
 async function getValidatedFunds(co) {
-    const [rows, fields] = await co.execute('SELECT SUM(amount*price_dollar) AS total_amount FROM dxd_transactions t INNER JOIN dxd_smartlinks k ON k.sender_addr = t.sender_addr INNER JOIN dxd_blockchain b ON b.tx_hash = t.tx_hash WHERE k.KYCStatus = "VALIDATED";').catch(error => {console.log(error)});
+    const [rows, fields] = await co.execute('SELECT SUM(amount*price_dollar) AS total_amount FROM dxd_transactions t INNER JOIN dxd_smartlinkcopy k ON k.sender_addr = t.sender_addr INNER JOIN dxd_blockchain b ON b.tx_hash = t.tx_hash WHERE k.KYCStatus = "APPROVED";').catch(error => {console.log(error)});
     return rows;
 }
 async function getValidatedAndPendingFunds(co) {
-    const [rows, fields] = await co.execute('SELECT SUM(amount*price_dollar) AS total_amount FROM dxd_transactions_unverified t INNER JOIN dxd_smartlinks k ON k.sender_addr = t.sender_addr INNER JOIN dxd_blockchain_unverified b ON b.tx_hash = t.tx_hash WHERE k.KYCStatus = "VALIDATED";').catch(error => {console.log(error)});
+    const [rows, fields] = await co.execute('SELECT SUM(amount*price_dollar) AS total_amount FROM dxd_transactions_unverified t INNER JOIN dxd_smartlinkcopy k ON k.sender_addr = t.sender_addr INNER JOIN dxd_blockchain_unverified b ON b.tx_hash = t.tx_hash WHERE k.KYCStatus = "APPROVED";').catch(error => {console.log(error)});
     return rows;
 }
 async function getAllFunds(co) {
-    const [rows, fields] = await co.execute('SELECT SUM(amount*price_dollar) AS total_amount FROM dxd_blockchain;').catch(error => {console.log(error)});
+    const [rows, fields] = await co.execute('SELECT SUM(amount*price_dollar) AS total_amount FROM dxd_blockchain_unverified;').catch(error => {console.log(error)});
+    return rows;
+}
+async function getSMAKtotal(co) {
+    const [rows, fields] = await co.execute('select sum(SMAK) AS SMAK from dxd_smartlinkcopy;').catch(error => {console.log(error)});
+    return rows;
+}
+async function getSMAKpresale(co) {
+    const [rows, fields] = await co.execute('select sum(SMAK_pre_sale) AS SMAK_pre_sale from dxd_smartlinkcopy;').catch(error => {console.log(error)});
+    return rows;
+}
+async function getSMAKico(co) {
+    const [rows, fields] = await co.execute('select sum(SMAK_ico) AS SMAK_ico from dxd_smartlinkcopy;').catch(error => {console.log(error)});
     return rows;
 }
 
@@ -44,8 +56,11 @@ async function task() {
     const validatedFunds = await getValidatedFunds(co);
     const validatedAndPendingFunds = await getValidatedAndPendingFunds(co);
     const allFunds = await getAllFunds(co);
+    const SMAKtotal = await getSMAKtotal(co);
+    const SMAKpresale = await getSMAKpresale(co);
+    const SMAKico = await getSMAKico(co);
     endDbConnection(co);
-    return [validatedFunds[0].total_amount, validatedAndPendingFunds[0].total_amount, allFunds[0].total_amount];
+    return [validatedFunds[0].total_amount, validatedAndPendingFunds[0].total_amount, allFunds[0].total_amount, SMAKtotal[0].SMAK, SMAKpresale[0].SMAK_pre_sale, SMAKico[0].SMAK_ico];
 }
 
 module.exports = router;
